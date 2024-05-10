802b43a50a391660b4515e6885e4b61bd3d60905
zachjhavers <zachjhavers@gmail.com>
2024-05-10 14:25:54 -0400

    Changed Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { React, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/NavBar";
 import PomodoroTimer from "./Components/Timer";
 import BreathingExercises from "./Components/Breathing";
 import Home from "./Components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ReactGA from "react-ga";
 
 const App = () => {
-  useEffect(() => {
-    ReactGA.initialize("G-65FMRK23PY");
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <BrowserRouter>
       <Navbar />
